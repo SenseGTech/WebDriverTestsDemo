1b20ae8a6edfcc6071e1c80bd70fccb867552b89
6thSense GoGiCH <6thSense GoGiCH@DESKTOP-FAO9L18>
2022-05-18 17:53:24 +0300

    Add project files.

---
+++ WebDriverTestsDemo/WikipediaTest.cs
@@ -0,0 +1,21 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+//Create new Chrome browser instance
+var driver = new ChromeDriver();
+
+//Navigate to wikipedia
+
+driver.Url = "https://wikipedia.org";
+
+System.Console.WriteLine("CURRENT TITLE: " + driver.Title);
+//Locate Search field by ID
+var searchField = driver.FindElement(By.Id("searchInput"));
+//Click on element
+searchField.Click();
+//Fill QA and press ENTER keyboard button
+searchField.SendKeys("Quality Assurance"  + Keys.Enter);
+System.Console.WriteLine("TITLE AFTER SEARCH: " + driver.Title);
+
+//Close browser
+driver.Quit();
